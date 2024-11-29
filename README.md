1737f29b54b270f14c0dc29d89f6a6b5c67f454b
Joshua Ward <joshua.l.ward@leidos.com>
2024-11-29 19:30:37 +0000

    Adding booleans for lazygit feature

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ This repo contains [Dev Container](https://containers.dev/overview)
 [Features](https://containers.dev/implementors/features/) for tools that I
 commonly use. Feel free to use them as well!
 
+## Documentation
+
+For auto-generating feature documentation, run the following from the `src` folder.
+
+```sh
+devcontainer features generate-docs -n j2udev/devcontainer-features
+```
+
 ## Testing
+
+For testing features, run the following from the root of the repo.
 
 ```sh
 devcontainer features test -i mcr.microsoft.com/devcontainers/base:jammy
--- src/lazygit/install.sh
+++ src/lazygit/install.sh
@@ -2,6 +2,8 @@
 set -e
 
 VERSION="${VERSION:-"0.44.1"}"
+ENABLE_CONFIG="${ENABLE_CONFIG:-"true"}"
+ENABLE_ALIAS="${ENABLE_ALIAS:-"true"}"
 
 detect_os_arch() {
     OS="$(uname | tr '[:upper:]' '[:lower:]')"
@@ -25,14 +27,17 @@ install_lazygit() {
 }
 
 configure_lazygit() {
-  mkdir -p "$_REMOTE_USER_HOME/.config/lazygit"
-  mkdir -p "$_CONTAINER_USER_HOME/.config/lazygit"
-  cp config.yml "$_REMOTE_USER_HOME/.config/lazygit/config.yml"
-  cp config.yml "$_CONTAINER_USER_HOME/.config/lazygit/config.yml"
-  echo "alias lzg='lazygit'" >> "$_REMOTE_USER_HOME/.bashrc"
-  echo "alias lzg='lazygit'" >> "$_REMOTE_USER_HOME/.zshrc"
-  echo "alias lzg='lazygit'" >> "$_CONTAINER_USER_HOME/.bashrc"
-  echo "alias lzg='lazygit'" >> "$_CONTAINER_USER_HOME/.zshrc"
+  if [ "$ENABLE_CONFIG" = "true" ]; then
+    mkdir -p "$_REMOTE_USER_HOME/.config/lazygit"
+    mkdir -p "$_CONTAINER_USER_HOME/.config/lazygit"
+    cp config.yml "$_REMOTE_USER_HOME/.config/lazygit/config.yml"
+    cp config.yml "$_CONTAINER_USER_HOME/.config/lazygit/config.yml"
+  fi
+  if [ "$ENABLE_ALIAS" = "true" ]; then
+    echo "alias lzg='lazygit'" >> "$_REMOTE_USER_HOME/.bashrc"
+    echo "alias lzg='lazygit'" >> "$_REMOTE_USER_HOME/.zshrc"
+    echo "alias lzg='lazygit'" >> "$_CONTAINER_USER_HOME/.bashrc"
+    echo "alias lzg='lazygit'" >> "$_CONTAINER_USER_HOME/.zshrc"
 }
 
 install_lazygit
