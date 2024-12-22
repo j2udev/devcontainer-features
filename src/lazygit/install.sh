64e1b7d8c3d0297425a42a00d518fa54d2ad1de4
Joshua Ward <j2udev@gmail.com>
2024-12-22 17:14:26 +0000

    chore: cleaning up lazygit feature

--- src/lazygit/install.sh
+++ src/lazygit/install.sh
@@ -2,24 +2,23 @@
 set -e
 
 detect_os_arch() {
-    OS="$(uname | tr '[:upper:]' '[:lower:]')"
-    ARCH="$(uname -m)"
+  OS="$(uname | tr '[:upper:]' '[:lower:]')"
+  ARCH="$(uname -m)"
 
-    case $ARCH in
-        x86_64) ARCH="amd64" ;;
-        armv8*|aarch64) ARCH="arm64" ;;
-        armv7*|armhf) ARCH="armv7" ;;
-        *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
-    esac
+  case $ARCH in
+      x86_64) ARCH="amd64" ;;
+      armv8*|aarch64) ARCH="arm64" ;;
+      armv7*|armhf) ARCH="armv7" ;;
+      *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+  esac
 
-    echo "${OS}_${ARCH}"
+  echo "${OS}_${ARCH}"
 }
 
 install_lazygit() {
-    local os_arch
-    os_arch=$(detect_os_arch)
-    local url="https://github.com/jesseduffield/lazygit/releases/download/v${VERSION}/lazygit_${VERSION}_${os_arch}.tar.gz"
-    curl -L "$url" | tar xzf - -C /usr/local/bin && chmod +x /usr/local/bin/lazygit
+  local os_arch=$(detect_os_arch)
+  local url="https://github.com/jesseduffield/lazygit/releases/download/v${VERSION}/lazygit_${VERSION}_${os_arch}.tar.gz"
+  curl -L "$url" | tar xzf - -C /usr/local/bin && chmod +x /usr/local/bin/lazygit
 }
 
 configure_lazygit() {
