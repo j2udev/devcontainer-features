31e7f1eb31882b3055ceebbf5eb0e4ccdd343295
Joshua Ward <joshua.l.ward@leidos.com>
2024-12-22 22:09:49 +0000

    feat: added oh-my-posh devcontainer feature

---
+++ src/oh-my-posh/README.md
@@ -0,0 +1,25 @@
+
+# FZF (fzf)
+
+Install FZF
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/j2udev/devcontainer-features/fzf:0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select a FZF version | string | 0.57.0 |
+| enable_config | Whether to create sane default config | boolean | true |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/oh-my-posh/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+detect_os_arch() {
+  OS="$(uname | tr '[:upper:]' '[:lower:]')"
+  ARCH="$(uname -m)"
+
+  case $ARCH in
+      x86_64) ARCH="amd64" ;;
+      armv8*|aarch64) ARCH="arm64" ;;
+      armv7*|armhf) ARCH="armv7" ;;
+      *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+  esac
+
+  echo "${OS}-${ARCH}"
+}
+
+install_oh_my_posh() {
+  local os_arch=$(detect_os_arch)
+  local url="https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v${VERSION}/posh-${os_arch}"
+  curl -Lo /usr/local/bin/oh-my-posh "$url" && chmod +x /usr/local/bin/oh-my-posh
+}
+
+configure_oh_my_posh() {
+  mkdir -p "${_REMOTE_USER_HOME}/.config/oh-my-posh"
+  mkdir -p "${_CONTAINER_USER_HOME}/.config/oh-my-posh"
+  cp "themes/${THEME}.omp.json" "${_REMOTE_USER_HOME}/.config/oh-my-posh"
+  cp "themes/${THEME}.omp.json" "${_CONTAINER_USER_HOME}/.config/oh-my-posh"
+  echo "eval \"$(oh-my-posh init zsh -c ${_REMOTE_USER_HOME}/.config/oh-my-posh/${THEME}.omp.json)\"" >> "$_REMOTE_USER_HOME/.zshrc"
+  echo "eval \"$(oh-my-posh init zsh -c ${_CONTAINER_USER_HOME}/.config/oh-my-posh/${THEME}.omp.json)\"" >> "$_CONTAINER_USER_HOME/.zshrc"
+}
+
+install_oh_my_posh
+configure_oh_my_posh
