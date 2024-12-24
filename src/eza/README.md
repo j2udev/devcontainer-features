dca8c1970e1ac7a5932a5ce61cd962d1bfa98006
Joshua Ward <j2udev@gmail.com>
2024-12-24 23:16:20 +0000

    feat: adding eza feature

---
+++ src/eza/README.md
@@ -0,0 +1,26 @@
+
+# eza (eza)
+
+Install eza
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/j2udev/devcontainer-features/eza:0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select an eza version | string | 0.20.13 |
+| theme | Select an eza theme | string | tokyonight |
+| enable_alias | Whether to create ls aliases for eza | boolean | true |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/eza/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -x
+
+VERSION="${VERSION:-"0.20.13"}"
+THEME="${THEME:-"tokyonight"}"
+
+install_eza() {
+  local os="$(uname | tr '[:upper:]' '[:lower:]')"
+  local arch="$(uname -m)"
+  local url="https://github.com/eza-community/eza/releases/download/v${VERSION}/eza_${arch}-unknown-${os}-gnu.tar.gz"
+  curl -L "$url" | tar xzf - -C /usr/local/bin && chmod +x /usr/local/bin/eza
+}
+
+configure_eza() {
+  mkdir -p "${_REMOTE_USER_HOME}/.config/eza"
+  mkdir -p "${_CONTAINER_USER_HOME}/.config/eza"
+  cp "themes/${THEME}.yml" "${_REMOTE_USER_HOME}/.config/eza/theme.yml"
+  cp "themes/${THEME}.yml" "${_CONTAINER_USER_HOME}/.config/eza/theme.yml"
+  if [ "$ENABLE_ALIAS" = "true" ]; then
+    echo "alias ls=eza" >> "${_REMOTE_USER_HOME}/.bashrc"
+    echo "alias ls=eza" >> "${_CONTAINER_USER_HOME}/.bashrc"
+    echo "alias ls=eza" >> "${_REMOTE_USER_HOME}/.zshrc"
+    echo "alias ls=eza" >> "${_CONTAINER_USER_HOME}/.zshrc"
+  fi
+}
+
+install_eza
+configure_eza
