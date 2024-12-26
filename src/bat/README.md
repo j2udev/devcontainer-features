b191f86e330ec06a753cdcb40aeebbd899b47fba
Joshua Ward <j2udev@gmail.com>
2024-12-26 16:09:23 +0000

    feat: adding bat feature

---
+++ src/bat/README.md
@@ -0,0 +1,25 @@
+
+# bat (bat)
+
+Install bat
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/j2udev/devcontainer-features/bat:0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select a bat version | string | 0.24.0 |
+| theme | Select a bat theme | string | default |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/bat/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+VERSION="${VERSION:-"0.24.0"}"
+THEME="${THEME:-"default"}"
+
+install_bat() {
+  local os="$(uname | tr '[:upper:]' '[:lower:]')"
+  local arch="$(uname -m)"
+  local url="https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat-v${VERSION}-${arch}-unknown-${os}-gnu.tar.gz"
+  curl -L "$url" | tar xzf - -C /opt && ln -s /opt/bat-v${VERSION}-${arch}-unknown-${os}-gnu/bat /usr/local/bin/bat
+}
+
+configure_bat() {
+  echo "alias bat='bat --theme=${THEME}'" >> "${_REMOTE_USER_HOME}/.bashrc"
+  echo "alias bat='bat --theme=${THEME}'" >> "${_CONTAINER_USER_HOME}/.bashrc"
+  echo "alias cat='bat -p --theme=${THEME}'" >> "${_REMOTE_USER_HOME}/.bashrc"
+  echo "alias cat='bat -p --theme=${THEME}'" >> "${_CONTAINER_USER_HOME}/.bashrc"
+  echo "alias bat='bat --theme=${THEME}'" >> "${_REMOTE_USER_HOME}/.zshrc"
+  echo "alias bat='bat --theme=${THEME}'" >> "${_CONTAINER_USER_HOME}/.zshrc"
+  echo "alias cat='bat -p --theme=${THEME}'" >> "${_REMOTE_USER_HOME}/.zshrc"
+  echo "alias cat='bat -p --theme=${THEME}'" >> "${_CONTAINER_USER_HOME}/.zshrc"
+}
+
+install_bat
+configure_bat
