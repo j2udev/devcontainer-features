833c4b646e2a00d30ab494ef209ded1eb6fd10b2
Joshua Ward <j2udev@gmail.com>
2024-12-27 20:08:43 +0000

    chore: trying to disable a default keybind for flash.nvim

--- src/lazyvim/plugins/flash.lua
+++ src/lazyvim/plugins/flash.lua
@@ -1,11 +1,7 @@
 return {
   "folke/flash.nvim",
-  lazy = false,
   -- removing the 'S' keybind as it conflicts with vim.surround motions
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "S", mode = { "n", "o", "x" }, false }
   },
 }
