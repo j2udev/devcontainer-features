94c567f911b6337875f77d8580bbd53eb4f1196d
Joshua Ward <j2udev@gmail.com>
2024-12-30 16:03:18 +0000

    feat: adding task feature and creating a Taskfile.yml

---
+++ src/task/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+VERSION="${VERSION:-"3.40.1"}"
+
+detect_os_arch() {
+  OS="$(uname | tr '[:upper:]' '[:lower:]')"
+  ARCH="$(uname -m)"
+
+  case $ARCH in
+      x86_64) ARCH="amd64" ;;
+      armv8*|aarch64) ARCH="arm64" ;;
+      *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+  esac
+
+  echo "${OS}_${ARCH}"
+}
+
+install_task() {
+  local os_arch=$(detect_os_arch)
+  local url="https://github.com/go-task/task/releases/download/v${VERSION}/task_${os_arch}.tar.gz"
+  curl -L "$url" | tar xzf - -C /usr/local/bin
+}
+
+install_task
