237553ca33fea83df3cea61b94118c170c4381c0
Joshua Ward <j2udev@gmail.com>
2025-01-02 21:37:26 +0000

    fix: updating completion for opentofu feature

--- test/opentofu/helpers.sh
+++ test/opentofu/helpers.sh
@@ -9,3 +9,8 @@ check_version() {
   tofu --version | grep 1.8.7 > /dev/null 2>&1
   return $?
 }
+
+check_completion() {
+  grep "complete -o nospace -C /usr/local/bin/tofu tofu" /home/vscode/.zshrc > /dev/null 2>&1
+  return $?
+}
