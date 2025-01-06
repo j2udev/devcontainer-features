00768444d6c87ea27ce6dc7a893f6d08488ba3bf
Joshua Ward <j2udev@gmail.com>
2025-01-06 18:14:20 +0000

    chore: cleaning up bat feature

--- src/bat/install.sh
+++ src/bat/install.sh
@@ -5,15 +5,22 @@ BIN_DIR="${BIN_DIR:-"/usr/local/bin"}"
 INSTALL_DIR="${INSTALL_DIR:-"/opt"}"
 VERSION="${VERSION:-"0.24.0"}"
 THEME="${THEME:-"default"}"
+_REMOTE_USER_HOME="${_REMOTE_USER_HOME:-"/home/vscode"}"
+_CONTAINER_USER_HOME="${_CONTAINER_USER_HOME:-"/home/vscode"}"
+
+shells=("zsh" "bash")
+homes=("${_REMOTE_USER_HOME}" "${_CONTAINER_USER_HOME}")
 
 get_os() {
-  local os="$(uname | tr '[:upper:]' '[:lower:]')"
+  local os
+  os="$(uname | tr '[:upper:]' '[:lower:]')"
   echo "${os}"
 }
 
 get_arch() {
-  local arch="$(uname -m)"
-  case "$arch" in
+  local arch
+  arch="$(uname -m)"
+  case "${arch}" in
       x86_64) arch="x86_64" ;;
       aarch64) arch="aarch64" ;;
       *) echo "Unsupported architecture: ${arch}"; exit 1 ;;
@@ -22,20 +29,19 @@ get_arch() {
 }
 
 install_bat() {
-  local url="https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat-v${VERSION}-$(get_arch)-unknown-$(get_os)-gnu.tar.gz"
-  curl -L "$url" | tar xzf - -C /opt && ln -s "${INSTALL_DIR}/bat-v${VERSION}-$(get_arch)-unknown-$(get_os)-gnu/bat" "${BIN_DIR}/bat"
-}
-
-install_aliases() {
-  echo "alias bat='bat --theme=${THEME}'" >> "$1"
-  echo "alias cat='bat -Pp --theme=${THEME}'" >> "$1"
+  local url
+  url="https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat-v${VERSION}-$(get_arch)-unknown-$(get_os)-gnu.tar.gz"
+  curl -L "${url}" | tar xzf - -C /opt \
+    && ln -sf "${INSTALL_DIR}/bat-v${VERSION}-$(get_arch)-unknown-$(get_os)-gnu/bat" "${BIN_DIR}/bat"
 }
 
 configure_bat() {
-  install_aliases "${_REMOTE_USER_HOME}/.bashrc"
-  install_aliases "${_CONTAINER_USER_HOME}/.bashrc"
-  install_aliases "${_REMOTE_USER_HOME}/.zshrc"
-  install_aliases "${_CONTAINER_USER_HOME}/.zshrc"
+  for shell in "${shells[@]}"; do
+    for home in "${homes[@]}"; do
+      echo "alias bat='bat --theme=${THEME}'" >> "${home}/.${shell}rc"
+      echo "alias cat='bat -Pp --theme=${THEME}'" >> "${home}/.${shell}rc"
+    done
+  done
 }
 
 install_bat
