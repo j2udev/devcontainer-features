0644a335d09d490fbb587493aeb6c337ccbd6de6
Joshua Ward <j2udev@gmail.com>
2025-01-14 16:07:31 +0000

    #!/bin/sh

--- src/skopeo/install.sh
+++ src/skopeo/install.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-_REMOTE_USER_HOME="${_REMOTE_USER_HOME:-"/home/vscode"}"
-_CONTAINER_USER_HOME="${_CONTAINER_USER_HOME:-"/home/vscode"}"
-
-configure_skopeo() {
-  mkdir -p "${1}/.config/containers"
-  jq -r '.credsStore' "${1}/.docker/config.json" | xargs -I{} echo 'credential-helpers = ["{}"]' > "${1}/.config/containers/registries.conf"
-}
-
 install_skopeo() {
   apt update && apt install -y skopeo
-  configure_skopeo "${_REMOTE_USER_HOME}"
-  configure_skopeo "${_CONTAINER_USER_HOME}"
-  configure_skopeo "${_REMOTE_USER_HOME}"
-  configure_skopeo "${_CONTAINER_USER_HOME}"
 }
 
 install_skopeo
